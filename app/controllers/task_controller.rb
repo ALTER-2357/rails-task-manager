3f5fae77083ca3999b82efbd0c8efc41a82f369f
lewis mills <lewismills290@protonmail.com>
2024-04-30 15:18:46 +0100

    //wip

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -4,45 +4,4 @@ class TasksController < ApplicationController
     def index
       @tasks = Task.all
     end
-<<<<<<< HEAD
 end
-=======
-  
-    def show
-    end
-  
-    def new
-      @task = Task.new
-    end
-  
-    def create
-      task = Task.new(task_params)
-      task.save
-      redirect_to task_path(task)
-    end
-  
-    def edit
-  
-    end
-  
-    def update
-      @task.update(task_params)
-      redirect_to task_path(@task)
-    end
-  
-    def destroy
-      @task.destroy
-      redirect_to tasks_path, status: :see_other
-    end
-  
-    private
-  
-    def task_params
-      params.require(:task).permit(:title, :details, :completed)
-    end
-  
-    def set_task
-      @task = Task.find(params[:id])
-    end
-  end
->>>>>>> 2d4e268b5e27f6246dddc7b6838935e0c6eca1fe
